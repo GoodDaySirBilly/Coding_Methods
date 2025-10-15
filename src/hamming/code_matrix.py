c071a654c3e0d9bb1f1990708581f334af72b98f
SayfovRK <rsayfov@bk.ru>
2025-10-15 22:24:19 +0300

    update interfaces of code_matrix

--- src/hamming/code_matrix.py
+++ src/hamming/code_matrix.py
@@ -1,40 +1,49 @@
 import numpy as np
+from ..galua import GaluaField
+
 
 '''
 File that has functions to build matricies by code params for another purposes 
 '''
 
-def build_generator_matrix(
-    code_length: int, # n
-    base_length: int  # k
-) -> np.ndarray[np.int32, np.int32]:
-    
-    '''
-    Return generator matrix G of linear block (n, k) code [k x n] size
-    '''
-
-    result = np.empty([base_length, code_length]) # sizes of result
-
-    return result
-
-
 def build_parity_check_matrix(
-    generator_matrix: np.ndarray[np.int32, np.int32] # G
+    code_length: int, # n
+    exss_length: int, # r
+    gf: GaluaField.GaluaField
 ) -> np.ndarray[np.int32, np.int32]:
     
     '''
     Return parity-check matrix H of linear block (n, k) code [r x n] size
     r = n - k
     '''
-    
-    # find shapes of G
-    base_length, code_length = generator_matrix.shape
-
-    # find nmber of excess symbols
-    exss_length = code_length - base_length
-
-
 
     result = np.empty([exss_length, code_length]) # sizes of result
 
     return result
+
+
+def build_generator_matrix(
+    parity_check_matrix: np.ndarray[np.int32, np.int32] # H
+) -> np.ndarray[np.int32, np.int32]:
+    
+    '''
+    Return generator matrix G of linear block (n, k) code [k x n] size
+    '''
+
+    
+    # find shapes of G
+    exss_length, code_length = parity_check_matrix.shape
+
+    # find nmber of excess symbols
+    base_length = code_length - exss_length
+
+    result = np.empty([base_length, code_length]) # sizes of result
+
+    return result
+
+def syndrom(
+    code_word: np.ndarray[np.int32],
+    parity_check_matrix: np.ndarray[np.int32, np.int32]
+) -> np.ndarray[np.int32]:
+    
+    return code_word @ parity_check_matrix.T
