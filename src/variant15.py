17c26cb511e646de5d062a0bb2d93b62d8b42c22
SayfovRK <rsayfov@bk.ru>
2025-10-15 17:53:53 +0300

    add var15

--- src/variant15.py
+++ src/variant15.py
@@ -13,12 +13,20 @@ gf2 = gf(11, 3, np.array([4, 1, 0, 1]))
 print(gf2)
 print(gf2.values)
 
+print("-------------------------------")
+print(gf1)
+
 # Creating elements of field
 el1 = el(gf1, np.array([1, 0, 0, 0, 1, 1]))
 el2 = el(gf1, np.array([1, 0, 0, 1, 0, 1]))
 
 one = el(gf1, np.array([1, 0, 0, 0, 0, 0]))
 zero = el(gf1, np.array([0, 0, 0, 0, 0, 0]))
+
+print("Element 1")
+print(el1)
+print("Element 2")
+print(el2)
 
 print("Sum of elements")
 print(el1 + el2)
@@ -41,3 +49,37 @@ print(el1 // one)
 print("Multiplying by zero")
 print(el1 * zero)
 
+print("-------------------------------")
+print(gf1)
+
+el1 = el(gf2, np.array([1, 4, 0]))
+el2 = el(gf2, np.array([3, 0, 2]))
+
+one = el(gf2, np.array([1, 0, 0]))
+zero = el(gf2, np.array([0, 0, 0]))
+
+print("Element 1")
+print(el1)
+print("Element 2")
+print(el2)
+
+print("Sum of elements")
+print(el1 + el2)
+
+print("Sub of elements")
+print(el1 - el2)
+
+print("Mul of elements")
+print(el1 * el2)
+
+print("Div of elements")
+print(el1 // el2)
+
+print("Multiplying by one")
+print(el1 * one)
+
+print("Division by one")
+print(el1 // one)
+
+print("Multiplying by zero")
+print(el1 * zero)
